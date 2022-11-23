c29282171b922b333c1d8601e6ddff26ff90d76f
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2022-11-23 16:51:57 -0700

    Refactor and thumbnails

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -51,71 +51,36 @@ class GridController < ApiController
 
     @index[:blocks] = new_blocks
 
-    #curl -L "https://..." -H "Authorization: token $token" --data ''
-
     #1. Get latest commit hash from the head of master
-    #curl -H "Authorization: token $token" -L https://api.github.com/repos/keaysma/fiarfli.art/git/refs/heads/master
-    #commit_url = res[:object][:url]
-    #commit_hash = res[:object][:sha]
-    uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/refs/heads/master")
-    req = Net::HTTP::Get.new(uri)
-    req["Authorization"] = "token #{token}"
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
-
-=begin
-    if res.is_a?(Net::HTTPUnauthorized)
-      render json: {
-        message: "Upstream Failure",
-        ustream_response: res_data
-      }
-      return
-    end
-=end
+    res_data = helpers.get(
+      "https://api.github.com/repos/keaysma/fiarfli.art/git/refs/heads/master",
+      token
+    )
     
     commit_url = res_data["object"]["url"]
     commit_hash = res_data["object"]["sha"]
 
     #2. Get the commit's data
-    #curl -H "Authorization: token $token" -L $commit_url
-    #tree_url = res[:tree][:url]
-    #tree_hash = res[:tree][:sha]
-    uri = URI(commit_url)
-    req = Net::HTTP::Get.new(uri)
-    req["Authorization"] = "token #{token}"
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
+    res_data = helpers.get(commit_url, token)
     tree_url = res_data["tree"]["url"]
     tree_hash = res_data["tree"]["sha"]
 
     #3. Get the tree
-    #curl -H "Authorization: token $token" -L "$(tree_url)?recursive=1"
-    #skipped - not needed
-    uri = URI("#{tree_url}?recursive=1")
-    req = Net::HTTP::Get.new(uri)
-    req["Authorization"] = "token #{token}"
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
+    res_data = helpers.get("#{tree_url}?recursive=1", token)
     tree_data = res_data["tree"]
 
-    puts "Get existing tree"
-    p tree_data
-    puts "\n"
+    puts "Existing tree:\n#{tree_data}\n"
 
     # locate filenames in the existing tree that are not used, add them to the tree as empty to remove them
-    new_art_nodes = new_blocks.map { |item| item["content"] }
-    new_art_nodes = new_art_nodes.flatten
-    new_art_nodes = new_art_nodes.map { |item| "public" + item["path"].sub(/\.[^.]*$/, '') }
+    new_art_nodes_index = new_blocks.map { |item| item["content"] }
+    new_art_nodes_index = new_art_nodes_index.flatten
+    new_art_nodes = new_art_nodes_index.map { |item| "public" + item["path"].sub(/\.[^.]*$/, '') }
+    # assume thumbnail will stay if it exists
+    new_art_nodes += new_art_nodes_index.map { |item| "public/thumbnail" + item["path"].sub(/\.[^.]*$/, '') }
     
-    existing_art_nodes = tree_data.select { |item| item["path"].match(/public\/art/) && item["type"] != "tree" }
-    existing_art_nodes = existing_art_nodes.map { |item| item["path"] }
-
+    existing_art_nodes_base = tree_data.select { |item| (item["path"].match(/public\/art/) || item["path"].match(/public\/thumbnail\/art/)) && item["type"] != "tree" }
+    existing_art_nodes = existing_art_nodes_base.map { |item| item["path"] }
+    
     remove_art_nodes = existing_art_nodes.select { |item| ! new_art_nodes.include? item.sub(/\.[^.]*$/, '') }
 
     puts "removing"
@@ -131,33 +96,27 @@ class GridController < ApiController
     }
 
     #4. Send the new file
-    #curl -H "Authorization: token $token" -X POST -L https://api.github.com/repos/keaysma/fiarfli.art/git/blobs --data '{"content": "{}"}'
-    #obj_url = res[:url]
-    #obj_hash = res[:sha]
     content_name_conversion = {}
+    thumbnails = {}
     content_tree = new_media.map! {|item| 
       item_name = item["name"]
       item_content = item["content"]
 
       if item_name.match(/\.(png|jpg|jpeg)$/) then
         webp_name = item["name"].sub(/\.[^.]*$/, '.webp')
+        webp_data = helpers.convert_to_webp(item_content)
+        
         p "#{item_name} -> #{webp_name}"
-
+        
         content_name_conversion[item_name] = webp_name
+        thumbnails[item_name] = webp_data[:thumbnail]
+
         item_name = webp_name
-        item_content = helpers.convert_to_webp(item["content"])
+        item_content = webp_data[:content]
       end
 
-      uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/blobs")
-      req = Net::HTTP::Post.new(uri)
-      req["Authorization"] = "token #{token}"
-      req["Content-Type"] = "application/json"
-
-      req.body = {"content": item_content, "encoding": "base64"}.to_json
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-        http.request(req)
-      }
-      res_data = JSON.parse(res.body)
+      
+      res_data = helpers.post("https://api.github.com/repos/keaysma/fiarfli.art/git/blobs", token, {"content": item_content, "encoding": "base64"}.to_json)
       
       ({
         "path": "public" + item_name,
@@ -167,15 +126,30 @@ class GridController < ApiController
       })
     }
 
-    puts "media uploaded"
-    p content_tree
-    puts "\n"
+    puts "media uploaded\n#{content_tree}\n"
 
     @index[:"blocks"] = @index[:"blocks"].map! {|block|
       block[:"content"] = block[:"content"].map! {|block_content|
-        new_name = content_name_conversion[block_content[:"path"]]
+        old_name = block_content[:"path"]
+        new_name = content_name_conversion[old_name]
         if !new_name.nil? then
           block_content[:"path"] = new_name
+
+          thumbnail_content = thumbnails[old_name]
+          if !thumbnail_content.nil? then
+            block_content[:"thumbnail"] = "thumbnail" + new_name
+
+            res_data = helpers.post(
+              "https://api.github.com/repos/keaysma/fiarfli.art/git/blobs", token, 
+              {"content": thumbnail_content, "encoding": "base64"}.to_json
+            )
+            content_tree += [{
+              "path": "public/thumbnail" + new_name,
+              "mode": "100644",
+              "type": "blob",
+              "sha": res_data["sha"]
+            }]
+          end
         end
         
         block_content
@@ -183,119 +157,71 @@ class GridController < ApiController
 
       block
     }
-    uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/blobs")
-    req = Net::HTTP::Post.new(uri)
-    req["Authorization"] = "token #{token}"
-    req["Content-Type"] = "application/json"
-    req.body = {"content": @index.to_json.to_s}.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
+
+    puts "thumbnails uploaded\n#{content_tree}\n"
+
+    res_data = helpers.post(
+      "https://api.github.com/repos/keaysma/fiarfli.art/git/blobs", token,
+      {"content": @index.to_json.to_s}.to_json
+    )
     obj_url = res_data["url"]
     obj_hash = res_data["sha"]
 
-    puts "index.json uploaded"
-    p res_data
-    p obj_hash
-    puts "\n"
+    puts "index.json uploaded\n#{res_data}\n#{obj_hash}\n"
 
-    uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/blobs")
-    req = Net::HTTP::Post.new(uri)
-    req["Authorization"] = "token #{token}"
-    req["Content-Type"] = "application/json"
-    req.body = {"content": @content.to_json.to_s}.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
+    res_data = helpers.post(
+      "https://api.github.com/repos/keaysma/fiarfli.art/git/blobs", token,
+      {"content": @content.to_json.to_s}.to_json
+    )
     content_url = res_data["url"]
     content_hash = res_data["sha"]
 
-    puts "content.json uploaded"
-    p res_data
-    p content_hash
-    puts "\n"
-
-    #Example data:
-    #{
-    #  "sha": "f78b831869101f29a05b0cc5c8ec395744090692",
-    #  "url": "https://api.github.com/repos/keaysma/fiarfli.art/git/blobs/f78b831869101f29a05b0cc5c8ec395744090692"
-    #}
-
-    # Send new content
-
+    puts "content.json uploaded\n#{res_data}\n#{content_hash}\n"
 
     #5. modify the recursive tree, replacing the "sha" and "url" values for the file with "path": "src/components/state.js"
     #just kidding we can skip this
 
     #6. Upload the new tree
-    #curl -H "Authorization: token $token" -X POST -L https://api.github.com/repos/keaysma/fiarfli.art/git/trees --data '{"tree": tree}'
-    #new_tree_url = res[:tree][:url]
-    #new_tree_hash = res[:tree][:sha]
-    uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/trees")
-    req = Net::HTTP::Post.new(uri)
-    req["Authorization"] = "token #{token}"
-    req["Content-Type"] = "application/json"
-    req.body = {
-      "base_tree": tree_hash,
-      "tree": [
-        {
-          "path": "src/components/index.json",
-          "mode": "100644",
-          "type": "blob",
-          "sha": obj_hash
-        },
-        {
-          "path": "src/components/content.json",
-          "mode": "100644",
-          "type": "blob",
-          "sha": content_hash
-        }
-      ] + content_tree + remove_content_tree
-    }.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
+    res_data = helpers.post(
+      "https://api.github.com/repos/keaysma/fiarfli.art/git/trees", token,
+      {
+        "base_tree": tree_hash,
+        "tree": [
+          {
+            "path": "src/components/index.json",
+            "mode": "100644",
+            "type": "blob",
+            "sha": obj_hash
+          },
+          {
+            "path": "src/components/content.json",
+            "mode": "100644",
+            "type": "blob",
+            "sha": content_hash
+          }
+        ] + content_tree + remove_content_tree
+      }.to_json
+    )
     new_tree_url = res_data["url"]
     new_tree_hash = res_data["sha"]
 
-    puts "tree uploaded"
-    p res_data
-    p new_tree_hash
-    puts "\n"
+    puts "tree uploaded\n#{res_data}\n#{new_tree_hash}\n"
 
     #7. Create the commit
-    #curl -H "Authorization: token $token" -X POST -L https://api.github.com/repos/keaysma/fiarfli.art/git/commits --data '{"message": "automated upload", "parents": [commit_hash], "tree": new_tree_hash}'
-    #new_commit_url = res[:tree][:url]
-    #new_commit_hash = res[:tree][:sha]
-    uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/commits")
-    req = Net::HTTP::Post.new(uri)
-    req["Authorization"] = "token #{token}"
-    req["Content-Type"] = "application/json"
-    req.body = {"message": "automated upload", "parents": [commit_hash], "tree": new_tree_hash}.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
-    res_data = JSON.parse(res.body)
+    res_data = helpers.post(
+      "https://api.github.com/repos/keaysma/fiarfli.art/git/commits", token,
+      {"message": "automated upload", "parents": [commit_hash], "tree": new_tree_hash}.to_json
+    )
     new_commit_url = res_data["url"]
     new_commit_hash = res_data["sha"]
 
-    puts "commit created"
-    p new_commit_hash
-    puts "\n"
+    puts "commit created\n#{new_commit_hash}\n"
 
     #8. Update head
-    #curl -H "Authorization: token $token" -X PATCH -L https://api.github.com/repos/keaysma/fiarfli.art/git/refs/heads/master --data '{"sha": new_commit_hash}'
-    uri = URI("https://api.github.com/repos/keaysma/fiarfli.art/git/refs/heads/master")
-    req = Net::HTTP::Post.new(uri)
-    req["Authorization"] = "token #{token}"
-    req["Content-Type"] = "application/json"
-    req.body = {"sha": new_commit_hash}.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-      http.request(req)
-    }
+    helpers.post(
+      "https://api.github.com/repos/keaysma/fiarfli.art/git/refs/heads/master", token,
+      {"sha": new_commit_hash}.to_json
+    )
 
     puts "head moved"
 
--- app/helpers/grid_helper.rb
+++ app/helpers/grid_helper.rb
@@ -6,26 +6,44 @@ require 'base64'
 require 'tempfile'
 
 module GridHelper
-    def http(method, path, headers, body)
+    def get(path, token)
         uri = URI(path)
-        req = Net::HTTP.method(method).new(uri)
+        req = Net::HTTP::Get.new(uri) #Net::HTTP.method(method).new(uri)
         
         # Hard coding headers for now
-        req["Authorization"] = headers["Authorization"]
+        req["Authorization"] = "token #{token}" #headers["Authorization"]
         res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| 
             http.request(req)
         }
 
-        res
+        res_data = JSON.parse(res.body)
+        res_data
+    end
+
+    def post(path, token, body)
+        uri = URI(path)
+        req = Net::HTTP::Post.new(uri)
+        
+        # Hard coding headers for now
+        req["Authorization"] = "token #{token}" #headers["Authorization"]
+        req.body = body
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| 
+            http.request(req)
+        }
+
+        res_data = JSON.parse(res.body)
+        res_data
     end
 
     def convert_to_webp(input_base64str)
         input_file = Tempfile.new(binmode: true)
         output_file = Tempfile.new(binmode: true)
+        thumbnail_file = Tempfile.new(binmode: true)
 
         input_content = Base64.decode64(input_base64str)
         input_file.write(input_content)
         input_file.flush()
+        input_file.close()
         #input_file.close()
 
         p input_base64str.length
@@ -37,7 +55,17 @@ module GridHelper
         output_file.rewind()
         output_content = output_file.read()
         output_base64str = Base64.encode64(output_content)
+        output_file.close()
 
-        output_base64str
+        webp_size = WebP.webp_size(output_content)
+        WebP.encode(input_file.path, thumbnail_file.path, quality: 95, resize_w: (webp_size[0] * 0.5).to_int, resize_h: (webp_size[1] * 0.5).to_int)
+        output_thumbnail = thumbnail_file.read()
+        output_thumbnail_base64str = Base64.encode64(output_thumbnail)
+        thumbnail_file.close()
+
+        ({
+            "content": output_base64str,
+            "thumbnail": output_thumbnail_base64str    
+        })
     end
 end
